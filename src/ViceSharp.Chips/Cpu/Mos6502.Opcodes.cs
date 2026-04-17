2a3abdce581f508a1f1004cd5c81b55f6fd06352
Sharp Ninja <payton@paytonbyrd.com>
2026-04-17 10:22:08 -0500

    Implement LDA/LDX/LDY, BRK/RTI/RTS, stack operations for 6502

--- src/ViceSharp.Chips/Cpu/Mos6502.Opcodes.cs
+++ src/ViceSharp.Chips/Cpu/Mos6502.Opcodes.cs
@@ -46,6 +46,99 @@ partial class Mos6502
 
     private void ExecuteOpcode(byte opcode)
     {
-        // Implementation will be completed as opcodes are added
+        switch (opcode)
+        {
+            // LDA - Load Accumulator
+            case 0xA9: A = _bus.Read(Immediate()); UpdateNZ(A); break;
+            case 0xA5: A = _bus.Read(ZeroPage()); UpdateNZ(A); break;
+            case 0xB5: A = _bus.Read(ZeroPageX()); UpdateNZ(A); break;
+            case 0xAD: A = _bus.Read(Absolute()); UpdateNZ(A); break;
+            case 0xBD: A = _bus.Read(AbsoluteX()); UpdateNZ(A); break;
+            case 0xB9: A = _bus.Read(AbsoluteY()); UpdateNZ(A); break;
+            case 0xA1: A = _bus.Read(IndirectX()); UpdateNZ(A); break;
+            case 0xB1: A = _bus.Read(IndirectY()); UpdateNZ(A); break;
+
+            // LDX - Load X Register
+            case 0xA2: X = _bus.Read(Immediate()); UpdateNZ(X); break;
+            case 0xA6: X = _bus.Read(ZeroPage()); UpdateNZ(X); break;
+            case 0xB6: X = _bus.Read(ZeroPageY()); UpdateNZ(X); break;
+            case 0xAE: X = _bus.Read(Absolute()); UpdateNZ(X); break;
+            case 0xBE: X = _bus.Read(AbsoluteY()); UpdateNZ(X); break;
+
+            // LDY - Load Y Register
+            case 0xA0: Y = _bus.Read(Immediate()); UpdateNZ(Y); break;
+            case 0xA4: Y = _bus.Read(ZeroPage()); UpdateNZ(Y); break;
+            case 0xB4: Y = _bus.Read(ZeroPageX()); UpdateNZ(Y); break;
+            case 0xAC: Y = _bus.Read(Absolute()); UpdateNZ(Y); break;
+            case 0xBC: Y = _bus.Read(AbsoluteX()); UpdateNZ(Y); break;
+
+            // NOP - No Operation
+            case 0xEA: break;
+
+            // BRK - Force Interrupt
+            case 0x00:
+                PushWord(PC);
+                Push(P);
+                P |= 0x04; // Set Interrupt Disable flag
+                PC = _bus.Read(0xFFFE);
+                PC |= (ushort)(_bus.Read(0xFFFF) << 8);
+                break;
+
+            // RTI - Return from Interrupt
+            case 0x40:
+                P = Pop();
+                PC = PopWord();
+                break;
+
+            // RTS - Return from Subroutine
+            case 0x60:
+                PC = PopWord();
+                PC++;
+                break;
+
+            default:
+                // Unimplemented opcode
+                break;
+        }
+    }
+
+    private void UpdateNZ(byte value)
+    {
+        P = (byte)(P & ~(0x02 | 0x80));
+        if (value == 0) P |= 0x02;
+        if ((value & 0x80) != 0) P |= 0x80;
+    }
+
+    private void SetFlag(byte mask)
+    {
+        P |= mask;
+    }
+
+    private void ClearFlag(byte mask)
+    {
+        P = (byte)(P & ~mask);
+    }
+
+    private void Push(byte value)
+    {
+        _bus.Write((ushort)(0x0100 + S--), value);
+    }
+
+    private byte Pop()
+    {
+        return _bus.Read((ushort)(0x0100 + ++S));
+    }
+
+    private void PushWord(ushort value)
+    {
+        Push((byte)(value >> 8));
+        Push((byte)value);
+    }
+
+    private ushort PopWord()
+    {
+        byte lo = Pop();
+        byte hi = Pop();
+        return (ushort)(lo | (hi << 8));
     }
 }
