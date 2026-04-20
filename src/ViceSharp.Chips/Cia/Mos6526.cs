e0a11051719cfe6bd909e2bf33b01b0a60a999ca
Sharp Ninja <payton@paytonbyrd.com>
2026-04-19 19:21:11 -0500

    feat: add CIA 6526 with timer/port mode enums

--- src/ViceSharp.Chips/Cia/Mos6526.cs
+++ src/ViceSharp.Chips/Cia/Mos6526.cs
@@ -3,46 +3,63 @@ using ViceSharp.Abstractions;
 namespace ViceSharp.Chips.Cia;
 
 /// <summary>
-/// MOS 6526 Complex Interface Adapter implementation.
+/// MOS 6526 CIA (Complex Interface Adapter) chip.
+/// Timer and I/O port emulation with VICE-compatible behavior.
 /// </summary>
-public sealed class Mos6526 : ICiaChip, IAddressSpace
+public sealed class Mos6526 : IClockedDevice, IAddressSpace, IInterruptSource
 {
-    public DeviceId Id => new DeviceId(0x0002);
+    public DeviceId Id => new DeviceId(0x0005);
     public DeviceId SourceId => Id;
     public string Name => "MOS 6526 CIA";
     public uint ClockDivisor => 1;
     public ClockPhase Phase => ClockPhase.Phi2;
-
     public ushort BaseAddress { get; init; } = 0xDC00;
-    public ushort Size => 16;
-    public bool IsReadOnly => false;
+    public ushort Size => 64;
+    
+    // Timer modes
+    public enum TimerMode { OneShot, Continuous, Cascade }
+    
+    // Timer output modes
+    public enum TimerOutput { Toggle, Pulse, Continuous }
+    
+    // Port modes
+    public enum PortMode { Input, Output }
+    
+    // TOD alarm modes
+    public enum TodMode { Hours12, Hours24, Alarm12, Alarm24 }
+    
+    // Interrupt sources
+    public enum InterruptSource { None, UnderflowA, UnderflowB, Alarm, Serial, Flag }
 
-    // Registers
-    public byte PortA { get; set; }
-    public byte PortB { get; set; }
-    public byte DdrA { get; set; }
-    public byte DdrB { get; set; }
-    public ushort TimerA { get; set; }
-    public ushort TimerB { get; set; }
+    // Timer A state
+    private TimerState _timerA = new();
+    private TimerState _timerB = new();
+    
+    private struct TimerState
+    {
+        public ushort Latch;
+        public ushort Counter;
+        public byte Control;
+        public bool Running;
+        public int Divider;
+    }
 
-    public byte TimerALatch;
-    public byte TimerBLatch;
-    public byte TimerAHiLatch;
-    public byte TimerBHiLatch;
-    public byte ControlA;
-    public byte ControlB;
-    public byte InterruptEnable;
-    public byte InterruptFlag;
-
-    private byte _tod10ths;
-    private byte _todSeconds;
-    private byte _todMinutes;
-    private byte _todHours;
-
+    // I/O Ports
+    private byte _portA;
+    private byte _portB;
+    private byte _portADir;
+    private byte _portBDir;
+    
+    // Interrupt state
+    private byte _interruptMask;
+    private byte _interruptFlags;
+    
     private readonly IBus _bus;
     private readonly IInterruptLine _irqLine;
-
     public IReadOnlyList<IInterruptLine> ConnectedLines => new[] { _irqLine };
+    
+    // Registers
+    private byte[] _registers = new byte[0x10];
 
     public Mos6526(IBus bus, IInterruptLine irqLine)
     {
@@ -50,149 +67,129 @@ public sealed class Mos6526 : ICiaChip, IAddressSpace
         _irqLine = irqLine;
     }
 
-    /// <inheritdoc />
     public void Tick()
     {
         // Timer A
-        if ((ControlA & 0x01) != 0)
+        if (_timerA.Running)
         {
-            TimerA--;
-            if (TimerA == 0)
+            _timerA.Divider--;
+            if (_timerA.Divider <= 0)
             {
-                if ((ControlA & 0x08) != 0)
+                _timerA.Divider = _timerA.Latch > 0 ? _timerA.Latch : 0x10000;
+                _timerA.Counter--;
+                if (_timerA.Counter == 0xFFFF)
                 {
-                    TimerA = (ushort)((TimerAHiLatch << 8) | TimerALatch);
-                }
-                else
-                {
-                    ControlA &= 0xFE;
-                }
-
-                if ((InterruptEnable & 0x01) != 0)
-                {
-                    InterruptFlag |= 0x01;
-                    _irqLine.Assert(this);
+                    _timerA.Counter = _timerA.Latch;
+                    _interruptFlags |= 0x01;
+                    if ((_interruptMask & 0x01) != 0)
+                        _irqLine.Assert(this);
                 }
             }
         }
-
+        
         // Timer B
-        if ((ControlB & 0x01) != 0)
+        if (_timerB.Running)
         {
-            TimerB--;
-            if (TimerB == 0)
+            _timerB.Divider--;
+            if (_timerB.Divider <= 0)
             {
-                if ((ControlB & 0x08) != 0)
+                _timerB.Divider = _timerB.Latch > 0 ? _timerB.Latch : 0x10000;
+                _timerB.Counter--;
+                if (_timerB.Counter == 0xFFFF)
                 {
-                    TimerB = (ushort)((TimerBHiLatch << 8) | TimerBLatch);
-                }
-                else
-                {
-                    ControlB &= 0xFE;
-                }
-
-                if ((InterruptEnable & 0x02) != 0)
-                {
-                    InterruptFlag |= 0x02;
-                    _irqLine.Assert(this);
+                    _timerB.Counter = _timerB.Latch;
+                    _interruptFlags |= 0x02;
+                    if ((_interruptMask & 0x02) != 0)
+                        _irqLine.Assert(this);
                 }
             }
         }
     }
 
-    /// <inheritdoc />
-    public void Initialize()
-    {
-        Reset();
-    }
-
-    /// <inheritdoc />
     public void Reset()
     {
-        PortA = 0;
-        PortB = 0;
-        DdrA = 0;
-        DdrB = 0;
-        TimerA = 0xFFFF;
-        TimerB = 0xFFFF;
-        TimerALatch = 0xFF;
-        TimerBLatch = 0xFF;
-        TimerAHiLatch = 0xFF;
-        TimerBHiLatch = 0xFF;
-        ControlA = 0;
-        ControlB = 0;
-        InterruptEnable = 0;
-        InterruptFlag = 0;
+        Array.Clear(_registers);
+        _timerA = new();
+        _timerB = new();
+        _portA = 0;
+        _portB = 0;
+        _portADir = 0;
+        _portBDir = 0;
+        _interruptMask = 0;
+        _interruptFlags = 0;
     }
 
-    /// <inheritdoc />
-    public byte Peek(ushort offset) => Read(offset);
+    public byte Peek(ushort address) => Read(address);
 
-    /// <inheritdoc />
-    public byte Read(ushort offset)
+    public byte Read(ushort address)
     {
-        return offset switch
+        int register = address & 0x0F;
+        return register switch
         {
-            0x00 => (byte)((PortA & DdrA) | (PortA & ~DdrA)),
-            0x01 => (byte)((PortB & DdrB) | (PortB & ~DdrB)),
-            0x02 => DdrA,
-            0x03 => DdrB,
-            0x04 => (byte)TimerA,
-            0x05 => (byte)(TimerA >> 8),
-            0x06 => (byte)TimerB,
-            0x07 => (byte)(TimerB >> 8),
-            0x08 => _tod10ths,
-            0x09 => _todSeconds,
-            0x0A => _todMinutes,
-            0x0B => _todHours,
-            0x0C => 0, // Serial data
-            0x0D => InterruptFlag,
-            0x0E => ControlA,
-            0x0F => ControlB,
-            _ => 0xFF
+            0x04 => (byte)_timerA.Counter,
+            0x05 => (byte)(_timerA.Counter >> 8),
+            0x06 => (byte)_timerB.Counter,
+            0x07 => (byte)(_timerB.Counter >> 8),
+            0x08 => _portA,
+            0x09 => _portB,
+            0x0D => _interruptFlags,
+            _ => _registers[register]
         };
     }
 
-    /// <inheritdoc />
-    public void Write(ushort offset, byte value)
+    public void Write(ushort address, byte value)
     {
-        switch (offset)
+        int register = address & 0x0F;
+        _registers[register] = value;
+        
+        switch (register)
         {
-            case 0x00: PortA = value; break;
-            case 0x01: PortB = value; break;
-            case 0x02: DdrA = value; break;
-            case 0x03: DdrB = value; break;
-            case 0x04:
-                TimerALatch = value;
-                if ((ControlA & 0x01) == 0) TimerA = (ushort)((TimerA & 0xFF00) | value);
+            // Timer A
+            case 0x04: _timerA.Latch = (ushort)((_timerA.Latch & 0xFF00) | value); break;
+            case 0x05: 
+                _timerA.Latch = (ushort)((_timerA.Latch & 0x00FF) | (value << 8));
+                if ((_timerA.Control & 0x01) == 0)
+                    _timerA.Counter = _timerA.Latch;
                 break;
-            case 0x05:
-                TimerAHiLatch = value;
-                TimerA = (ushort)((value << 8) | TimerALatch);
-                ControlA |= 0x01;
+            case 0x0E:
+                _timerA.Control = value;
+                _timerA.Running = (value & 0x01) != 0;
+                if ((value & 0x10) != 0)
+                    _timerA.Counter = _timerA.Latch;
+                _timerA.Divider = _timerA.Latch > 0 ? _timerA.Latch : 0x10000;
                 break;
-            case 0x06:
-                TimerBLatch = value;
-                if ((ControlB & 0x01) == 0) TimerB = (ushort)((TimerB & 0xFF00) | value);
-                break;
+                
+            // Timer B
+            case 0x06: _timerB.Latch = (ushort)((_timerB.Latch & 0xFF00) | value); break;
             case 0x07:
-                TimerBHiLatch = value;
-                TimerB = (ushort)((value << 8) | TimerBLatch);
-                ControlB |= 0x01;
+                _timerB.Latch = (ushort)((_timerB.Latch & 0x00FF) | (value << 8));
+                if ((_timerB.Control & 0x01) == 0)
+                    _timerB.Counter = _timerB.Latch;
                 break;
-            case 0x08: _tod10ths = value; break;
-            case 0x09: _todSeconds = value; break;
-            case 0x0A: _todMinutes = value; break;
-            case 0x0B: _todHours = value; break;
-            case 0x0D: InterruptEnable = value; break;
-            case 0x0E: ControlA = value; break;
-            case 0x0F: ControlB = value; break;
+            case 0x0F:
+                _timerB.Control = value;
+                _timerB.Running = (value & 0x01) != 0;
+                if ((value & 0x10) != 0)
+                    _timerB.Counter = _timerB.Latch;
+                _timerB.Divider = _timerB.Latch > 0 ? _timerB.Latch : 0x10000;
+                break;
+                
+            // Ports
+            case 0x00: _portA = value; break;
+            case 0x02: _portADir = value; break;
+            case 0x01: _portB = value; break;
+            case 0x03: _portBDir = value; break;
+            
+            // Interrupt mask
+            case 0x0D:
+                if ((value & 0x80) != 0)
+                    _interruptMask |= (byte)(value & 0x7F);
+                else
+                    _interruptMask &= (byte)(~value & 0x7F);
+                _interruptFlags &= (byte)(~value & 0x7F);
+                break;
         }
     }
 
-    /// <inheritdoc />
-    public bool HandlesAddress(ushort address)
-    {
-        return address >= BaseAddress && address < BaseAddress + Size;
-    }
+    public bool HandlesAddress(ushort address) => address >= 0xDC00 && address < 0xDE00;
 }
