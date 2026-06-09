d37846962a023c990b8b4ad9f7f884a1d44ea526
Sharp Ninja <ninja@thesharp.ninja>
2026-06-08 21:34:52 -0500

    perf(bus): fast-path C64 memory map dispatch

--- src/ViceSharp.Core/BasicBus.cs
+++ src/ViceSharp.Core/BasicBus.cs
@@ -10,14 +10,21 @@ public sealed class BasicBus : IBus
 {
     private List<IAddressSpace> _devices = new();
     private readonly object _lock = new();
+    private C64MemoryMap? _singleC64MemoryMap;
 
     // PERF-BUS-001: Read/Write/Peek run on the single emulation thread (SystemClock);
     // no lock needed here. RegisterDevice/UnregisterDevice retain their lock for
     // safe setup/teardown from any thread.
     public byte Read(ushort address)
     {
-        foreach (var device in _devices)
+        if (_singleC64MemoryMap is { } c64MemoryMap)
         {
+            return c64MemoryMap.Read(address);
+        }
+
+        for (var i = 0; i < _devices.Count; i++)
+        {
+            var device = _devices[i];
             if (device.HandlesAddress(address))
                 return device.Read(address);
         }
@@ -26,8 +33,15 @@ public sealed class BasicBus : IBus
 
     public void Write(ushort address, byte value)
     {
-        foreach (var device in _devices)
+        if (_singleC64MemoryMap is { } c64MemoryMap)
         {
+            c64MemoryMap.Write(address, value);
+            return;
+        }
+
+        for (var i = 0; i < _devices.Count; i++)
+        {
+            var device = _devices[i];
             if (device.HandlesAddress(address))
             {
                 device.Write(address, value);
@@ -38,8 +52,14 @@ public sealed class BasicBus : IBus
 
     public byte Peek(ushort address)
     {
-        foreach (var device in _devices)
+        if (_singleC64MemoryMap is { } c64MemoryMap)
         {
+            return c64MemoryMap.Peek(address);
+        }
+
+        for (var i = 0; i < _devices.Count; i++)
+        {
+            var device = _devices[i];
             if (device.HandlesAddress(address))
                 return device.Peek(address);
         }
@@ -51,6 +71,7 @@ public sealed class BasicBus : IBus
         lock (_lock)
         {
             _devices.Insert(0, device);
+            RefreshFastPath();
         }
     }
 
@@ -59,6 +80,14 @@ public sealed class BasicBus : IBus
         lock (_lock)
         {
             _devices.Remove(device);
+            RefreshFastPath();
         }
+    }
+
+    private void RefreshFastPath()
+    {
+        _singleC64MemoryMap = _devices.Count == 1 && _devices[0] is C64MemoryMap memoryMap
+            ? memoryMap
+            : null;
     }
 }
